388a3ac15333c73b1ed80ce73760e5a5f1e76f7d
darling-rockstar-4852 <daisyrene483@gmail.com>
2019-02-05 00:04:18 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,25 @@
-
+def valid move?(board,index)
+def position_taken?(board,index)
+  if(board[index]==" ")||board[index]==" "||board[index]==nil
+    taken=false
+  else
+    taken=true
+  end
+end
+def on_board?(number)
+  if number.between?(0,8)==true
+    return true
+  else
+    return false
+  end
+end
+if (position_taken?(board,index)=false&&(on_board?)(index)=true
+  return true
+else
+  return false
+end
+end
+    
     
 
   
